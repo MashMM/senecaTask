f3c900fd4e468150d144dff29263331d432d825f
MashMM <discordowner159@gmail.com>
2024-09-22 09:53:56 +0100

    setup express server

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Seneca stats service');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
