d9eabcbcb331ee9da1e5853153e18a56fea1dd56
MashMM <discordowner159@gmail.com>
2024-09-23 02:20:34 +0100

    configured environment variables for test database

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,9 +1,20 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
+import dotenv from 'dotenv';
 
 export async function getDatabase() {
+
+  dotenv.config();
+  const dbUrl = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
+  if (!dbUrl) {
+    throw new Error('Database URL is not defined in environment variables');
+  }
+
   const db = await open({
-    filename: './database.db',
+    filename: dbUrl,
     driver: sqlite3.Database,
   });
 
